21c36ba1010859d8f518b57181cfab573695fca0
Olexiy Koval <kalex32@ukr.net>
2018-11-19 14:45:14 +0200

    Complete HW_JS_Random_19_11_18

--- ScriptRandom.js
+++ ScriptRandom.js
@@ -1,7 +1,17 @@
-var num = Math.round(Math.random()*100);
-console.log("Number:", num);
+var num = Math.round(Math.random() * 100);
+// console.log("Number:", num);
 
-var guessedNum = prompt("Введите число от 0 до 100", '');
-if (guessedNum == num){
-    alert("Поздравляем!");
+var guessedNum = 0;
+
+for (var count = 0; guessedNum != num; count++) {
+    guessedNum = prompt("Введите число от 0 до 100", '');
+    if (guessedNum === null) {
+        alert("Выход")
+        throw new Error();
+    } else if (guessedNum > num) {
+        alert("Ваше число больше загаданного");
+    } else if (guessedNum < num) {
+        alert("Ваше число меньше загаданного");
+    }
 }
+alert("Поздравляем! Вы угадали число за " + count + " попыток!");
