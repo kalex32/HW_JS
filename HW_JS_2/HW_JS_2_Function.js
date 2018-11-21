543367d5a80c2cad83322cc219d1f1085fe852b1
Olexiy Koval <kalex32@ukr.net>
2018-11-21 23:24:10 +0200

    Proceed HW_JS_2

---
+++ HW_JS_2/HW_JS_2_Function.js
@@ -0,0 +1,19 @@
+console.log(array_clone([1,2,4,0]));
+console.log(array_clone([1,2,[4,0]]));
+
+function array_clone(arr) {
+	arr.slice(0)
+	return arr;
+}
+
+function summ_kv() {
+	return arguments.length;
+}
+
+function array_shuffle(arr) {
+	return arr;
+}
+
+function unique(arr) {
+	return arr;
+}
