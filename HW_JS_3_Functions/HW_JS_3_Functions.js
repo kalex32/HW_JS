366fac6a23c77a3af65abb1f04b8d33c4ca4717a
Olexiy Koval <kalex32@ukr.net>
2018-12-07 01:17:21 +0200

    Palindrom complete

--- HW_JS_3_Functions/HW_JS_3_Functions.js
+++ HW_JS_3_Functions/HW_JS_3_Functions.js
@@ -1,2 +1,18 @@
 console.log(isPalindrom("длинношеее"));
-console.log(isPalindrom()"мадам");
+console.log(isPalindrom("мадам"));
+console.log(isPalindrom("портос"));
+console.log(isPalindrom("адаппада"));
+
+function isPalindrom(str) {
+    var result = "";
+    for (var i = 0; i < str.length / 2; i++) {
+        if (str[i] === str[str.length - i - 1]) {
+            result = "Это палиндром!";
+        } else {
+            result = "Это не палиндром!";
+            return result;
+
+        }
+    }
+    return result;
+}
